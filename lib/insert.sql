f84d414c211d0ad36014cb3dce777de3acaf65c6
Mike Dilley <mike.dilley@gmail.com>
2018-11-26 17:15:37 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,7 +1,7 @@
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "tan", "angry", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Rowdy", 1, "F", "butterscotch", "friendly", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "tan", "angry", 1);
-INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "tan", "angry", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "F", "brown", "supes nice", 1);
+INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 8, "M", "dark brown", "serious", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "tan", "angry", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "tan", "angry", 1);
 INSERT INTO bears(name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "M", "tan", "angry", 1);
